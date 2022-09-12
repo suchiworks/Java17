2cfb8d7fdf73abd4840c46f9a1bf3260e6995e23
suchiworks <111557185+suchiworks@users.noreply.github.com>
2022-09-12 13:50:16 +0530

    Add files via upload

--- 12-sep-22/Mismatch.java
+++ 12-sep-22/Mismatch.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 
 public class Mismatch {
 
-	
+// Write a program to throw a user-defined exception "String Mismatch Exception", if
+	//two strings are not equal (ignore the case).//	
 public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter the string :: ");
+		System.out.print("Enter the string ");
 		String input = sc.nextLine();
 		
 		try {
 			if(input.equalsIgnoreCase("Hello"))
-				System.out.println("String matched !!!");
+				System.out.println("String matched !");
 			else
 				throw new StringMismatchException();
 		}
